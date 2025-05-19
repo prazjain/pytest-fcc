1bc1f303dd68630d424594ba81457f4277aca5fc
Prashant Jain <praz.jain@gmail.com>
2025-05-19 16:20:30 -0400

    test fixture example

--- source/shapes.py
+++ source/shapes.py
@@ -22,6 +22,11 @@ class Rectangle(Shape):
         self.width = width
         self.height = height
 
+    def __eq__(self, other):
+        if not isinstance(other, Rectangle):
+            return False
+        return self.width == other.width and self.height == other.height
+
     def area(self):
         return self.width * self.height
 
--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -2,17 +2,30 @@ import pytest
 from source.shapes import Rectangle
 import math
 
+@pytest.fixture
+def my_rectangle():
+    """Fixture to create a Rectangle object."""
+    rectangle = Rectangle(10, 5)
+    yield rectangle
+    del rectangle
+
+@pytest.fixture
+def invalid_rectangle():
+    """Fixture to create an invalid Rectangle object."""
+    rectangle = Rectangle(-10, 5)
+    yield rectangle
+    del rectangle
+
 class TestRectangle:
-    def setup_method(self, method):
-        self.rectangle = Rectangle(10, 5)
-        print("Setup for test method:", method.__name__)
 
-    def teardown_method(self, method):
-        print("Teardown for test method:", method.__name__)
-        del self.rectangle 
+    def test_rectangle_area(self, my_rectangle):
+        assert my_rectangle.area() == my_rectangle.width * my_rectangle.height
 
-    def test_rectangle_area(self):
-        assert self.rectangle.area() == self.rectangle.width * self.rectangle.height
+    def test_rectangle_perimeter(self, my_rectangle):
+        assert my_rectangle.perimeter() == 2 * (my_rectangle.width + my_rectangle.height)
 
-    def test_rectangle_perimeter(self):
-        assert self.rectangle.perimeter() == 2 * (self.rectangle.width + self.rectangle.height)
+    def test_rectangle_equality(self, my_rectangle, invalid_rectangle):
+        assert my_rectangle == Rectangle(10, 5)
+        assert my_rectangle != invalid_rectangle
+        assert my_rectangle != "Not a rectangle"
+        assert my_rectangle != Rectangle(5, 10)
